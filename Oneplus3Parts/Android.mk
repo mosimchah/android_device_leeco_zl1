972fe3dea98149d41cf79ef62e4d0fde42b4a23a
mosimchah <mosimchah@gmail.com>
2017-02-13 14:44:51 -0500

    attempt to fix Oneplus3 parts (imported from oppo common)

--- Oneplus3Parts/Android.mk
+++ Oneplus3Parts/Android.mk
@@ -4,10 +4,16 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := Oneplus3Parts
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.cyanogenmod.platform.internal
+
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res
 
 package_resource_overlays := $(strip \
     $(wildcard $(foreach dir, $(PRODUCT_PACKAGE_OVERLAYS), \
